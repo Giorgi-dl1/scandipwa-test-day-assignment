ed5caf7d0c99daed3a93d377bd8a12ac4185fbad
Giorgi-dl1 <96057077+Giorgi-dl1@users.noreply.github.com>
2022-10-04 18:03:43 +0400

    fix rendering

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,6 +1,8 @@
 import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component';
 import Progressbar from 'src/components/Progressbar';
 import './Checkout.extension.style.scss';
+import ContentWrapper from 'Component/ContentWrapper';
+
 class Checkout extends SourceCheckout {
   renderProgressbar() {
     const { checkoutStep } = this.props;
@@ -9,6 +11,30 @@ class Checkout extends SourceCheckout {
       return { name: item[0], displayValue: item[1].title.value.split(' ')[0] };
     });
     return <Progressbar steps={steps} currentStep={checkoutStep} />;
+  }
+  render() {
+    return (
+      <main block="Checkout">
+        {this.renderProgressbar()}
+        <ContentWrapper
+          wrapperMix={{ block: 'Checkout', elem: 'Wrapper' }}
+          label={__('Checkout page')}
+        >
+          {this.renderSummary(true)}
+          <div block="Checkout" elem="Step">
+            {this.renderTitle()}
+            {this.renderGuestForm()}
+            {this.renderStep()}
+            {this.renderLoader()}
+          </div>
+          <div>
+            {this.renderSummary()}
+            {this.renderPromo()}
+            {this.renderCoupon()}
+          </div>
+        </ContentWrapper>
+      </main>
+    );
   }
 }
 
